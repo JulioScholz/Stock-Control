3e9a033e0560ea2f62fc1d8db2c26e9995fc775e
joaocalixto1004 <joao.calixto1004@gmail.com>
2019-12-09 14:52:46 -0300

    Update Stock.java

--- src/Stock.java
+++ src/Stock.java
@@ -24,10 +24,47 @@ public class Stock {
     }
 
     public void new_product(String name, int code, int initial_qty) {
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection = connectionClass.getConnection();
+        PreparedStatement myStmt = null;
+        ResultSet myRs = null;
 
+        myStmt = connection.preparedStatement("INSERT INTO PRODUCTS (nome, code, quantidade, custo, preco, " +
+                "dataentrada, perecivel, fornecedor, lote, localizacao, dataValidade) VALUES (? ,? ,? ,? ,?, ?, ?, ?, ?, ?, ?)");
+
+        myStmt.setString(1, name);
+        myStmt.setInt(2, code);
+        myStmt.setInt(3, initial_qty);
+        myStmt.setDouble(4, custo);
+        myStmt.setDouble(5, preco);
+        myStmt.setDate(6, java.sql.Date.valueOf(java.time.LocalDate.now()));
+        myStmt.setBoolean(7, perecivel);
+        myStmt.setString(8, fornecedor);
+        myStmt.setString(9, lote);
+        myStmt.setString(10, localizacao);
+        myStmt.setDate(11, java.sql.Date.valueOf(java.time.LocalDate.now()));
+
+        myRs = myStmt.executeUpdate();
+
+        display(myRs);
     }
 
     public Product update_qty(String product,  int _qty) {
+        /***
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection = connectionClass.getConnection();
+        PreparedStatement myStmt = null;
+        ResultSet myRs = null;
+
+        myStmt = connection.preparedStatement("UPDATE PRODUCTS SET quantidade = ? WHERE id = ?");
+
+        myStmt.setInt(1, new_qty);
+        myStmt.setInt(2, product);
+        myRs = myStmt.executeUpdate();
+        
+        
+        
+        */
         Iterator<Product> itr = productList.iterator();
 
         while(itr.hasNext()) {
@@ -44,6 +81,15 @@ public class Stock {
     }
 
     public boolean search() {
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection = connectionClass.getConnection();
+
+        private String sql = "SELECT * FROM products WHERE cpf=" + cpf + ";";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        if (resultSet){
+            return true;
+        }
         return false;
     }
 
